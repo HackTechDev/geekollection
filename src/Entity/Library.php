c971f39bd510bd4b35e96bab45d77b5d0b8e5beb
Nekrofage <lesanglierdesardennes@gmail.com>
2023-10-25 19:05:27 +0200

    [Update] Form Library entity

--- src/Entity/Library.php
+++ src/Entity/Library.php
@@ -6,6 +6,7 @@ use App\Repository\LibraryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 #[ORM\Entity(repositoryClass: LibraryRepository::class)]
 class Library
@@ -20,6 +21,14 @@ class Library
 
     #[ORM\ManyToMany(targetEntity: Item::class, inversedBy: 'libraries')]
     private Collection $item;
+
+    #[Gedmo\Timestampable(on:"update")]
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $created_at;
+ 
+    #[Gedmo\Timestampable(on:"update")]
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $updated_at;
 
     public function __construct()
     {
@@ -77,6 +86,31 @@ class Library
     {
         $this->item->removeElement($item);
 
+        return $this;
+    }
+
+    
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+ 
+    public function setCreatedAt(?\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+ 
+        return $this;
+    }
+ 
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+ 
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+ 
         return $this;
     }
 }
