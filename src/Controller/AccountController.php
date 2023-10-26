5aedcf03637d17efb2b21449f1710826986a0cef
Nekrofage <lesanglierdesardennes@gmail.com>
2023-10-26 22:33:26 +0200

    [Add] Account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DashboardController extends AbstractController
+class AccountController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('/account', name: 'app_account')]
     public function index(): Response
     {
-        return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+        return $this->render('account/index.html.twig', [
+            'controller_name' => 'AccountController',
         ]);
     }
 }
